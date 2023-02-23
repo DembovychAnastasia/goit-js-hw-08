4fec7e02625d93617d9979cb0b40d8c6a7ef9297
DembovychAnastasia <113368687+DembovychAnastasia@users.noreply.github.com>
2023-02-23 16:46:37 +0200

    update gallery 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
+// Описаний в документації
+import SimpleLinpnghtbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+console.log(container);
+const itemsMarkup = createGalleryItems(galleryItems);
+
+function createGalleryItems(items) {
+  return items.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  }).join('');
+}
+container.insertAdjacentHTML('beforeend', itemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+  });
+
+
+
